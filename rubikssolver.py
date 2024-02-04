ccc21f8f2f9e3c752ca78c6bea9daeb10bed5f20
Holden Casey <me@holdencasey.com>
2024-02-03 23:45:22 -0500

    astar only

--- rubikssolver.py
+++ rubikssolver.py
@@ -1,22 +1,8 @@
 from searchAgent import SearchAgent, Rubiks2x2SearchProblem, rubiks2x2FacesHeuristic
-from search import dfs, bfs, astar, ucs
-import sys
+from search import astar
 
 if __name__ == '__main__':
-    _, search = sys.argv
-    if search == 'dfs':
-        searchFunc = dfs
-    elif search == 'bfs':
-        searchFunc = bfs
-    elif search == 'astar':
-        searchFunc = astar
-    elif search == 'ucs':
-        searchFunc = ucs
-    else:
-        print("Please enter search function as argument")
-        sys.exit()
-    
-    print("Using Search:", search)
+    print("Using A* Search:")
     print("For faces, input lowercase first letter of color in order TL TR BL BR (reading order)")
     upFace = tuple(list(input("Up Face: ")))
     frontFace = tuple(list(input("Front Face: ")))
@@ -26,7 +12,7 @@ if __name__ == '__main__':
     downFace = tuple(list(input("down Face: ")))
 
     scrambledState = (upFace, frontFace, leftFace, rightFace, backFace, downFace)
-    searchAgent = SearchAgent(searchFunc, Rubiks2x2SearchProblem, rubiks2x2FacesHeuristic)
+    searchAgent = SearchAgent(astar, Rubiks2x2SearchProblem, rubiks2x2FacesHeuristic)
     searchAgent.registerInitialState(scrambledState)
     actions = searchAgent.getActions()
     print(actions)
